c0d36cd5c042c7ebb2327bfaf1e338c0f31675a9
debankar <rdebankar@gmail.com>
2020-07-28 21:02:10 +0530

    teacher_home page added enroll exam modal added

--- database.sql
+++ database.sql
@@ -13,3 +13,30 @@ create table users(
     token VARCHAR(255) NOT NULL unique,
     PRIMARY KEY (id)
 );
+
+
+create table enrolled_exams(
+	id int(200) NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL unique,
+    enrolled_paper VARCHAR(255) NOT NULL,
+    paper_code VARCHAR(255) NOT NULL,
+    score VARCHAR(255) NOT NULL,
+    PRIMARY KEY (email)
+);
+
+create table papers(
+	id int(200) NOT NULL AUTO_INCREMENT,
+    paper VARCHAR(255) NOT NULL,
+    paper_code VARCHAR(255) NOT NULL,
+    PRIMARY KEY (paper_code)
+);
+
+create table enrolled_students(
+	id int(200) NOT NULL AUTO_INCREMENT,
+    student_email VARCHAR(255) NOT NULL,
+    student_name VARCHAR(255) NOT NULL,
+    paper VARCHAR(255) NOT NULL,
+    paper_code VARCHAR(255) NOT NULL,
+    PRIMARY KEY (paper_code)
+);
+
